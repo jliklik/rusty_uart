545cd4c85dcd9a1913d689b868bae8fa574cab47
Jian Lik Ng <jianlik.ng@gmail.com>
2023-08-14 22:30:05 -0700

    Echo program works, but rather slow. Btw, green wire on TTL adapter goes to pin 10, white wire goes to pin 9

--- src/main.rs
+++ src/main.rs
@@ -7,20 +7,25 @@ use nb::block;
 use cortex_m_rt::entry;
 use cortex_m_semihosting::hprintln;
 use panic_halt as _;
-use cortex_m::asm;
+// use cortex_m::asm;
 use stm32f1xx_hal::{
     pac::{self, USART1}, 
+    gpio::{Pin,Alternate},
     prelude::*,
     serial::{Config, Serial}
 }; // STM32F1 hardware abstraction layer crate
 
+// fn echo(serial: &mut Serial<USART1, (Pin<'A',9,Alternate>, Pin<'A',10>)>) {
+//     let sent = b'X';
+//     let received = block!(serial.rx.read()).unwrap();
+//     hprintln!("Received: {}", received);
+//     block!(serial.tx.write(sent)).unwrap();  
+// }
+
 #[entry]
 fn main() -> ! {
 
-    // let p = cortex_m::Peripherals::take().unwrap();
-    // let mut itm = p.ITM;
-    // iprintln!(&mut itm.stim[0], "Hello, world!");
-    hprintln!("The quick brown fox jumps over the lazy dog");
+    hprintln!("Starting program");
 
     let dp = pac::Peripherals::take().unwrap();
     let mut flash = dp.FLASH.constrain();
@@ -47,45 +52,15 @@ fn main() -> ! {
         dp.USART1,
         (pin_tx, pin_rx),
         &mut afio.mapr, // alternate function map
-        Config::default()
-            .baudrate(9600.bps()),
-            // .wordlength_9bits()
-            // .parity_none(),
+        Config::default().baudrate(9600.bps()),
         &clocks,
     );
 
-    //let usart1: &mut USART1 = unsafe { &mut *(USART1::ptr() as *mut _) };
-    // Separate into tx and rx channels
-    //let (mut tx, mut rx) = serial.split();
-
-    hprintln!("Entering loop!");
-
-    let sent = b'X';
-    block!(serial.tx.write(sent)).unwrap();
-
-    // Read the byte that was just sent. Blocks until the read is complete
-    let received = block!(serial.rx.read()).unwrap();
-
-    // Since we have connected tx and rx, the byte we sent should be the one we received
-    assert_eq!(received, sent);
-    hprintln!("Sent: {}", sent);
-    hprintln!("Received: {}", received);
-
-    // Trigger a breakpoint to allow us to inspect the values
-    asm::bkpt();
-
     loop {
-        // hprintln!("Inside loop!");
-        //let received = (rx.read_u16()).unwrap(); // this blocks
-        //tx.write_u16(received).unwrap();
-
-        // while usart1.sr.read().rxne().bit_is_clear() {}
-        // let byte = usart1.dr.read().dr().bits() as u8;
-
-        let byte = block!(serial.rx.read()).unwrap();
-
-        hprintln!("Received: {}", byte);
-        
+        // echo
+        let received = block!(serial.rx.read()).unwrap();
+        hprintln!("Received: {}", received);
+        block!(serial.tx.write(received)).unwrap();
     }
 
 }
